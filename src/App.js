721b6fb5106ff56de739f43a7e71f2035c451e0b
Andrey Slesarchuk <andrey.slesarchuk@gmail.com>
2019-06-07 12:06:38 +0300

    Simple example from Leaflet

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import './App.css';
-//import map from "./components/Map/Map";
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-        <map className="map-container" />
-    </div>
-  );
+import {Map, TileLayer, Marker, Popup} from 'react-leaflet';
+
+import './App.css';
+
+class App extends Component {
+    state = {
+        lat: 53.90000,
+        lng: 27.56667,
+        zoom: 7,
+    }
+    render() {
+        const position = [this.state.lat, this.state.lng]
+        return (
+            <Map className="map" center={position} zoom={this.state.zoom}>
+                <TileLayer
+                    attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={position}>
+                    <Popup>
+                        A pretty CSS3 popup. <br/> Easily customizable.
+                    </Popup>
+                </Marker>
+            </Map>
+        );
+    }
 }
+
 export default App;
